6fe42aa0e44276f4ab7f19bec120c02f2f7fdf41
Luiz melo <you@example.com>
2023-01-02 10:09:48 -0300

    Exercícios do curso da alura explorando C#

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o Projeto 5 - caracteres e textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        string primeiraFrase = "Alura cursos de tecnologia ";
+        Console.WriteLine(primeiraFrase + 2022);
+
+        string vazia = "";
+        Console.WriteLine(vazia);
+
+        string cursos = @"Cursos disponíveis: 
+- Go
+- C#
+- Python
+- Java";
+        Console.WriteLine(cursos);
+
+        Console.ReadLine();
+    }
+
+}
---
+++ P11-CalculaPoupaca2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 11 - Calcula Poupança");
+
+        // rendimento de 0.5% ao mês
+
+        /*
+        while (mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("no mês " + mes + " você tem " + investimento);
+
+            mes += 1;
+        }
+        */
+        double investimento = 1000;
+
+        for (int mes = 1; mes <= 12; mes = mes++)
+        {
+            investimento *= 1.005;
+            Console.WriteLine("no mês " + mes + " você tem R$ " + investimento);
+        }
+
+        Console.ReadLine();
+    }
+}
